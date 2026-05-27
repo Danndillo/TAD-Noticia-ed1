716a28a57fe3cbdcd74362d641746b95190ef36d
平和Danllo <121500063+Danndillo@users.noreply.github.com>
2026-05-27 08:11:33 -0300

    Add Noticia.h header file with data structures

--- Noticia.h
+++ Noticia.h
@@ -1,1 +1,33 @@
+#ifndef NOTICIA_H
+#define NOTICIA_H 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct data {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef enum tipoClassificacao {
+    EMANALISE = 1,
+    CONFIAVEL,
+    SUSPEITA
+} TypeClass;
+
+typedef struct noticia{
+    int id;
+    Data data;
+    char titulo[50];
+    char conteudo[500];
+    char fonte[100];
+    TypeClass tipo;
+} Noticia;
+
+Noticia* criarNoticia(int newId, Data newData, char newTitulo[], char newConteudo[], char newFonte[], TypeClass newClassificacao);
+void imprimirNoticia(Noticia* n);
+Noticia* liberarNoticia(Noticia* n);
+
+#endif //NOTICIA_H
