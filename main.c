b9f99a949756bcfb1deb3b9a3a4e2768eab7d8a6
平和Danllo <121500063+Danndillo@users.noreply.github.com>
2026-05-27 08:12:06 -0300

    Implement main menu for news management

--- main.c
+++ main.c
@@ -1,1 +1,59 @@
-//main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include "Noticia.h"
+#include "ListaCabCau.h"
+
+//setlocale(LC_ALL, "pt-br");
+
+int main(){
+
+    int opt=0;
+    Lista cabCau;
+    criarLista(&cabCau);
+
+    while(opt !=8){
+        printf("----------------MENU------------------\n");
+        printf("1 - Adicionar nova noticia\n");
+        printf("2 - Remover notícia pendente por id\n");
+        printf("3 - Remover notícia verificada por palavra-chave\n");
+        printf("4 - Buscar noticias pendentes por palavra-chave\n");
+        printf("5 - Imprimir as noticias\n");
+        printf("6 - Classificar notícias\n");
+        printf("7 - Imprimir quantidade de notícias por categoria\n");
+        printf("8 - Sair\n");
+        printf("Escolha uma opção:\n");
+        printf("-------------------------------------------\n\n> ");
+
+        scanf("%d", &opt); 
+
+        switch(opt){
+            case 1:
+                printf("legal");
+                break;
+
+            /*case :
+            case :
+            case :
+            case :
+            case :
+            case :*/
+            case 8:
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    }
+    /*
+    Data temp = {.dia = 1, .mes = 1, .ano = 2005};
+    Noticia *novaNot = criarNoticia(1, temp, "Escala 6x1", "Bluebird Bluebird Bluebird Bluebird Bluebird", "G1", 4);
+    
+    inserirNoticiaInicio(&cabCau, *novaNot);
+    liberarNoticia(novaNot);
+    novaNot = criarNoticia(2, temp, "Escala 5x2", "Helter Skelter", "Uol", 1);
+    inserirNoticiaInicio(&cabCau, *novaNot);
+    imprimirLista(&cabCau);*/
+    return 0;
+}
